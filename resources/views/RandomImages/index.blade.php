99cbc98ac4290ef0127f645ac7b11eb7a4d6bdb4
Naitsabes <jsperezsalazar2001@gmail.com>
2020-11-14 13:49:55 -0500

    Updating view v2

--- resources/views/RandomImages/index.blade.php
+++ resources/views/RandomImages/index.blade.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <h1> Take a look of this image ........ </h1>
-    <h5>Server IP:  {{ $data['ip'] }} </h5>
-    <p>{{ $data['storage']->url($data['image_name']) }}</p>
+    <p>Image URL: {{ $data['storage']->url($data['image_name']) }}</p>
     <img src="{{ $data['storage']->url($data['image_name']) }}" alt="">
+    <h5>Server IP:  {{ $data['ip'] }} </h5>
 </body>
 </html>
